c2fd2abbf9329e1c2610244f181f8e1b008b55c8
Taylor Romero <me@taylorrome.ro>
2024-08-30 11:38:33 -0600

    major: exported class name change

--- src/index-module.ts
+++ src/index-module.ts
@@ -1,7 +1,7 @@
 export { default as CrudMasterSkillViewController } from './master/CrudMasterSkillViewController'
 export * from './master/CrudMasterSkillViewController'
 
-export { default as CrudMasterListCardViewController } from './master/CrudListCardViewController'
+export { default as CrudListCardViewController } from './master/CrudListCardViewController'
 export * from './master/CrudListCardViewController'
 
 export { default as crudAssert } from './assertions/crudAssert'
