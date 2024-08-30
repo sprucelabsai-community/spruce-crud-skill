a53f3cb305b96e1a910191d2fae664df662a09e9
Taylor Romero <me@taylorrome.ro>
2024-08-30 11:55:31 -0600

    patch: missed exports

--- src/index-module.ts
+++ src/index-module.ts
@@ -4,4 +4,11 @@ export * from './master/CrudMasterSkillViewController'
 export { default as CrudMasterListCardViewController } from './master/CrudListCardViewController'
 export * from './master/CrudListCardViewController'
 
+export { default as CrudDetailSkillViewController } from './detail/CrudDetailSkillViewController'
+export * from './detail/CrudDetailSkillViewController'
+
+export { default as CrudDetailFormCardViewController } from './detail/CrudDetailFormCardViewController'
+export * from './detail/CrudDetailFormCardViewController'
+
 export { default as crudAssert } from './assertions/crudAssert'
+export * from './assertions/crudAssert'
